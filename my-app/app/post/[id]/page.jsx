7fcf1c9fe9d204175c2fdafe524bb1769566f07f
JoshuaAlbuquerque <125178226+JoshuaAlbuquerque@users.noreply.github.com>
2024-07-14 19:01:35 +0530

    I have no idea how to do the last task

--- my-app/app/post/[id]/page.jsx
+++ my-app/app/post/[id]/page.jsx
@@ -1,6 +1,19 @@
-"use client";
-import { usePathname } from "next/navigation";
-export default function Post() {
-const pathname = usePathname();
-return <p> {pathname}</p>;// as far as i can tell we are passing the path of the file we want so we can have one code do it dynamically
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h1>
+      Joshua Quinthino Albuquerque
+    </h1>
+    <p>
+      I an very tired
+    </p>
+    <p>
+      So far it fine but im not good at CSS so i have no idea what was supposed to break in a previous task
+    </p>
+    </>
+  )
 }
+
+export default page
